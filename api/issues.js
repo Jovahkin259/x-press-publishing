6cbf439a897acb2966778cbf65f5a7493bc879ab
Joshua Astle <jovahkin259@gmail.com>
2021-03-18 23:24:06 +0000

    add GET all issues for a given series

--- api/issues.js
+++ api/issues.js
@@ -1,4 +1,20 @@
 const express = require('express')
-const issuesRouter = express.Router()
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite')
+const issuesRouter = express.Router({ mergeParams: true })
+
+// Get all existing issues
+issuesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Issue WHERE Issue.series_id = $seriesId', {
+    $seriesId: req.params.seriesId
+  },
+  (error, issues) => {
+    if (error) {
+      next(error)
+    } else {
+      res.status(200).json({ issues: issues })
+    }
+  })
+})
 
 module.exports = issuesRouter
