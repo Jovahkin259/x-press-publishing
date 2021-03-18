8e755826f417c4d26ea73199703eaf386858691f
Joshua Astle <jovahkin259@gmail.com>
2021-03-18 19:53:23 +0000

    Add GET all series

--- api/series.js
+++ api/series.js
@@ -3,4 +3,13 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite')
 const seriesRouter = express.Router()
 
+seriesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Series', (error, series) => {
+    if (error) {
+      next(error)
+    } else {
+      res.status(200).json({ series: series })
+    }
+  })
+})
 module.exports = seriesRouter
