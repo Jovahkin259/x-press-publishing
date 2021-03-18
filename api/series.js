612733c5ecf28b9b5cf013d7c0ac892925e3b1be
Joshua Astle <jovahkin259@gmail.com>
2021-03-18 19:57:21 +0000

    add seriesId param handling

--- api/series.js
+++ api/series.js
@@ -3,6 +3,21 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite')
 const seriesRouter = express.Router()
 
+// Check seriesId
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+  db.get(`SELECT * FROM Series WHERE id = ${seriesId}`, (error, series) => {
+    if (error) {
+      next(error)
+    } else if (series) {
+      req.series = series
+      next()
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
+
+// Get all series
 seriesRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Series', (error, series) => {
     if (error) {
