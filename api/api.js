120ea0491f1cc63f5ebd283382c2e90f2ceabb0c
Joshua Astle <jovahkin259@gmail.com>
2021-03-18 16:45:58 +0000

    Create artistsRouter and mount

--- api/api.js
+++ api/api.js
@@ -1,4 +1,5 @@
 const express = require('express')
 const apiRouter = express.Router()
+const artistsRouter = require('./artists')
 
 module.exports = apiRouter
---
+++ api/artists.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const artistsRouter = express.Router()
+
+module.exports = artistsRouter
