24b9b9fa1df2656da5d3c057e6f3c5b12577c672
Joshua Astle <jovahkin259@gmail.com>
2021-03-18 16:48:29 +0000

    Import and add instance of sqlite3 database

--- api/artists.js
+++ api/artists.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite')
 const artistsRouter = express.Router()
 
 module.exports = artistsRouter
