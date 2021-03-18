31a58b9236ed0b1907723b5af6b8ca49f64c0a73
Joshua Astle <jovahkin259@gmail.com>
2021-03-18 17:42:25 +0000

    Add POST create new artist

--- api/artists.js
+++ api/artists.js
@@ -4,6 +4,18 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite
 
 const artistsRouter = express.Router()
 
+// Validate new artist
+const validateArtist = (req, res, next) => {
+  const artist = req.body.artist
+  if (!artist.name || !artist.dateOfBirth || !artist.biography) {
+    res.sendStatus(404)
+  }
+  if (!artist.isCurrentlyEmployed) {
+    req.body.artist.isCurrentlyEmployed = 1
+  }
+  next()
+}
+
 // Check artist ID parameter
 artistsRouter.param('artistId', (req, res, next, artistId) => {
   db.get('SELECT * FROM artist WHERE id = $artistId',
@@ -36,4 +48,27 @@ artistsRouter.get('/:artistId', (req, res, next) => {
   res.status(200).json({ artist: req.artist })
 })
 
+// Create a new artist
+artistsRouter.post('/', validateArtist, (req, res, next) => {
+  db.run('INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed)' +
+        'VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed )', {
+    $name: req.body.artist.name,
+    $dateOfBirth: req.body.artist.dateOfBirth,
+    $biography: req.body.artist.biography,
+    $isCurrentlyEmployed: req.body.artist.isCurrentlyEmployed
+  },
+  function (error) {
+    if (error) {
+      next(error)
+    } else {
+      db.get(`SELECT * FROM Artist WHERE Artist.id = ${this.lastID}`, (error, artist) => {
+        if (error) {
+          next(error)
+        }
+        res.status(201).json({ artist: artist })
+      })
+    }
+  }
+  )
+})
 module.exports = artistsRouter
